f4aa19762742582b48259cd838a1d61b10a0b8c7
wangliang13 <wangliang13@lenovo.com>
2016-04-09 21:49:29 +0800

    add linked list node definatioin

---
+++ linked_list.c
@@ -0,0 +1,14 @@
+//singly linked list node
+struct SingleList
+{
+	int val;
+	SingleList* pNext;	
+}
+
+//double linked list node
+struct DoubleList
+{
+	int val;
+	DoubleList* pNext;
+	DoubleList* pPrev;
+}
