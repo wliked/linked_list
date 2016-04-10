1d6133cbb42b0b026ec6c4081e2658d67ff74883
wangliang13 <wangliang13@lenovo.com>
2016-04-10 12:59:37 +0800

    add: reverse single linked list

--- linked_list.c
+++ linked_list.c
@@ -2,13 +2,40 @@
 struct SListNode
 {
 	int val;
-	SListNode* pNext;	
+	SListNode* next;	
 }
 
 //double linked list node
 struct DListNode
 {
 	int val;
-	DListNode* pNext;
-	DListNode* pPrev;
+	DListNode* next;
+	DListNode* prev;
+}
+
+//reverse single linked list
+SListNode* reverse_single_linked_list(SListNode* head)
+{
+	SListNode* slist_node=NULL;
+	SListNode* slist_node_prev=NULL;
+	SListNode* slist_node_next=NULL;
+	
+	if(head==NULL)
+	{
+		return head;
+	}
+	
+	slist_node = head->next;
+	slist_node_prev = head;
+	
+	while(slist_node)
+	{
+	    slist_node_next =slist_node->next;
+		slist_node->next=slist_node_prev;
+		slist_node_prev=slist_node;
+		slit_node=slist_node_next;
+	}	
+	
+	head->next = NULL;
+	return  slist_node_prev;
 }
